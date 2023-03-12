8d642d7be77adf1e255d364daef67e549a67e99f
AmazingBuff <lhq499445413@163.com>
2023-03-12 23:34:41 +0800

    new file:   dataStructure/reference/queue.h 	new file:   dataStructure/reference/stack.h 	modified:   dataStructure/reference/linkedList.h 	modified:   dataStructure/reference/sequentialList.h 	modified:   dataStructure/textbook/BasicVariable.h

---
+++ dataStructure/reference/queue.h
@@ -0,0 +1,309 @@
+#include"../textbook/BasicVariable.h"
+#include"stack.h"
+
+template<typename T>
+struct CircularQueue
+{
+private:
+    T* data;
+    uint32_t front_;
+    uint32_t rear_;
+    uint32_t length;
+public:
+    CircularQueue(uint32_t size = 10) : front_(0), rear_(0), length(size)
+    {
+        data = new T[size];
+    }
+
+    ~CircularQueue()
+    {
+        delete[] data;
+    }
+
+    void enqueue(T& value)
+    {
+        assert(!full());
+        data[rear_] = value;
+        rear_ = (rear_ + 1) % length;
+    }
+
+    void enqueue(T&& value)
+    {
+        assert(!full());
+        data[rear_] = value;
+        rear_ = (rear_ + 1) % length;
+    }
+
+    void dequeue()
+    {
+        assert(!empty());
+        front_ = (front_ + 1) % length;
+    }
+
+    T front() const
+    {
+        assert(!empty());
+        return data[front_];
+    }
+
+    bool empty() const
+    {
+        return front_ == rear_;
+    }
+
+    bool full() const
+    {
+        return (rear_ + 1) % length == front_;
+    }
+
+    uint32_t size() const
+    {
+        if (front_ > rear_)
+            return rear_ + length - front_;
+        else
+            return rear_ - front_;
+    }
+
+    uint32_t capacity() const
+    {
+        return length;
+    }
+};
+
+
+template<typename T>
+struct Queue
+{
+private:
+    List<T>* front_ = nullptr;
+    List<T>* rear_ = nullptr;
+    uint32_t length = 0;
+public:
+    Queue()
+    {
+        front_ = new List<T>;
+        rear_ = front_;
+    }
+
+    ~Queue()
+    {
+        delete front_;
+    }
+
+    void enqueue(T& value)
+    {
+        List<T>* newNode = new List<T>(value);
+        rear_->next = newNode;
+        rear_ = newNode;
+        length++;
+    }
+
+    void enqueue(T&& value)
+    {
+        List<T>* newNode = new List<T>(value);
+        rear_->next = newNode;
+        rear_ = newNode;
+        length++;
+    }
+
+    void dequeue()
+    {
+        assert(!empty());
+        List<T>* delNode = front_->next;
+        front_->next = delNode->next;
+        if(rear_ == delNode)
+            rear_ = front_;
+        delete delNode;
+        length--;
+    }
+
+    T front() const
+    {
+        assert(!empty());
+        return front_->next->data;
+    }
+
+    bool empty() const
+    {
+        return front_ == rear_;
+    }
+
+    uint32_t size() const
+    {
+        return length;
+    }
+
+    void destroy()
+    {
+        while(!empty());
+            dequeue();
+    }
+};
+
+
+
+//2.
+void reverseQueue(Queue<char>& queue)
+{
+    Stack<char> stack;
+    while(!queue.empty())
+    {
+        stack.push(queue.front());
+        queue.dequeue();
+    }
+    while(!stack.empty())
+    {
+        queue.enqueue(stack.top());
+        stack.pop();
+    }
+}
+
+//3.
+template<typename T>
+struct QueueFromStack
+{
+private:
+    Stack<T> in;
+    Stack<T> out;
+public:
+    T front()
+    {
+        assert(!empty());
+        if(out.empty())
+        {
+            while(!in.empty())
+            {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+
+    void enqueue(T& value)
+    {
+        in.push(value);
+    }
+
+    void enqueue(T&& value)
+    {
+        in.push(value);
+    }
+
+    void dequeue()
+    {
+        assert(!empty());
+        if(out.empty())
+        {
+            while(!in.empty())
+            {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        out.pop();
+    }
+
+    bool empty() const
+    {
+        return in.empty() && out.empty();
+    }
+
+    uint32_t size() const
+    {
+        return in.size() + out.size();
+    }
+};
+
+//4.
+template<typename T>
+struct LinkCircularQueue
+{
+private:
+    List<T>* front_ = nullptr;
+    List<T>* rear_ = nullptr;
+    uint32_t length = 0;
+    uint32_t space = 0;
+public:
+    LinkCircularQueue()
+    {
+        front_ = new List<T>;
+        rear_ = front_;
+        rear_->next = front_;
+    }
+
+    ~LinkCircularQueue()
+    {
+        delete front_;
+    }
+
+    void enqueue(T& value)
+    {
+        if (full())
+        {
+            List<T>* newNode = new List<T>;
+            newNode->next = rear_->next;
+            rear_->data = value;
+            rear_->next = newNode;
+            rear_ = newNode;
+            space++;
+        }
+        else
+        {
+            rear_->data = value;
+            rear_ = rear_->next;
+        }
+        length++;
+    }
+
+    void enqueue(T&& value)
+    {
+        if (full())
+        {
+            List<T>* newNode = new List<T>;
+            newNode->next = rear_->next;
+            rear_->data = value;
+            rear_->next = newNode;
+            rear_ = newNode;
+            space++;
+        }
+        else
+        {
+            rear_->data = value;
+            rear_ = rear_->next;
+        }
+        length++;
+    }
+
+    void dequeue()
+    {
+        assert(!empty());
+        front_ = front_->next;
+        length--;
+    }
+
+    T front() const
+    {
+        return front_->data;
+    }
+
+    bool empty() const
+    {
+        return front_ == rear_;
+    }
+
+    bool full() const
+    {
+        return rear_->next == front_;
+    }
+
+    uint32_t size() const
+    {
+        return length;
+    }
+
+    uint32_t capacity() const
+    {
+        return space;
+    }
+};
