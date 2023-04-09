051d8b941064e938d18de44be1b21553cd71119c
AmazingBuff <lhq499445413@163.com>
2023-04-09 23:37:32 +0800

    modified:   source/search.hpp

--- source/search.hpp
+++ source/search.hpp
@@ -212,13 +212,13 @@ private:
 };
 
 template<typename T>
-struct AVLTreeNode : public TreeNode<T>
+struct AVLTreeNode
 {
     int balance_factor = 0;
-    explicit AVLTreeNode(const T& element)
-    {
-        this->data = element;
-    }
+    T data = 0;
+    AVLTreeNode* left = nullptr;
+    AVLTreeNode* right = nullptr;
+    explicit AVLTreeNode(const T& element) : data(element) {}
     AVLTreeNode() = default;
 };
 
@@ -231,6 +231,12 @@ struct AVLTree
     BlanceSortTree* root = nullptr;
 
     AVLTree() = default;
+    ~AVLTree()
+    {
+        if(root)
+            destroy();
+        root = nullptr;
+    }
 
     AVLTree(const std::initializer_list<Hash>& list)
     {
@@ -241,89 +247,18 @@ struct AVLTree
     void insert(const Ty_Key& key, const Ty_Val& value)
     {
         Hash hash(key, value);
-        insert(hash, root);
+        insert(hash, root, root);
     }
 
     void insert(const Hash& element)
     {
-        insert(element, root);
+        insert(element, root, root);
     }
 
     void erase(const Ty_Key& key)
     {
         assert(root != nullptr);
-        if(root->data.key == key)
-        {
-            if(root->left != nullptr && root->right != nullptr)
-            {
-                BlanceSortTree* left_child = root->left;
-                BlanceSortTree* right_child = root->right;
-                BlanceSortTree* rightest_child = left_child;
-                //find the rightest node of the left subtree of cur
-                while(rightest_child->right != nullptr)
-                    rightest_child = rightest_child->right;
-                delete root;
-                root = left_child;
-                rightest_child->right = right_child;
-            }
-            else if(root->left != nullptr || root->right != nullptr)
-            {
-                BlanceSortTree* child = root->left ? root->left : root->right;
-                delete root;
-                root = child;
-            }
-            else
-            {
-                delete root;
-                root = nullptr;
-            }
-        }
-        else
-        {
-            BlanceSortTree* prev = root;
-            BlanceSortTree* cur = prev;
-            find_both(key, root, cur, prev);
-
-            if(cur != nullptr && prev != nullptr)
-            {
-                bool left_children = cur == prev->left;
-
-                if(cur->left != nullptr && cur->right != nullptr)
-                {
-                    BlanceSortTree* cur_left_child = cur->left;
-                    BlanceSortTree* cur_right_child = cur->right;
-                    BlanceSortTree* rightest_child = cur_left_child;
-                    //find the rightest node of the left subtree of cur
-                    while(rightest_child->right != nullptr)
-                        rightest_child = rightest_child->right;
-
-                    delete cur;
-
-                    if(left_children)
-                        prev->left = cur_left_child;
-                    else
-                        prev->right = cur_left_child;
-                    rightest_child->right = cur_right_child;
-                }
-                else if(cur->left != nullptr || cur->right != nullptr)
-                {
-                    BlanceSortTree* cur_child = cur->left ? cur->left : cur->right;
-                    delete cur;
-                    if(left_children)
-                        prev->left = cur_child;
-                    else
-                        prev->right = cur_child;
-                }
-                else
-                {
-                    delete cur;
-                    if(left_children)
-                        prev->left = nullptr;
-                    else
-                        prev->right = nullptr;
-                }
-            }
-        }
+        find_min_unbalance_subtree();
     }
 
     BlanceSortTree* find(const Ty_Key& key) const
@@ -357,6 +292,16 @@ struct AVLTree
     }
 
 private:
+    struct Info
+    {
+        int prev_depth;
+        int cur_depth;
+        bool operator==(const Info& other) const
+        {
+            return prev_depth == other.prev_depth && cur_depth == other.cur_depth;
+        }
+    };
+
     void find_both(const Ty_Key& key, BlanceSortTree* node, BlanceSortTree*& cur, BlanceSortTree*& prev)
     {
         if(node == nullptr)
@@ -382,15 +327,121 @@ private:
         }
     }
 
-    void insert(const Hash& element, BlanceSortTree*& node)
+    BlanceSortTree* find_min_unbalance_subtree(BlanceSortTree* node) const
     {
         if(node == nullptr)
+            return nullptr;
+    }
+
+    //four rotation
+    void right_to_left()
+    {
+
+    }
+
+    void left_to_right()
+    {
+
+    }
+
+    void left_to_left()
+    {
+
+    }
+
+    void right_to_right()
+    {
+
+    }
+
+    Info insert(const Hash& element, BlanceSortTree*& node, BlanceSortTree*& prev)
+    {
+        if(node == nullptr)
+        {
             node = new BlanceSortTree(element);
+            if(node == prev)
+                return {0, 0};
+            else
+            {
+                if(node == prev->left)
+                    prev->balance_factor += 1;
+                else
+                    prev->balance_factor -= 1;
+                return {0, 1};
+            }
+        }
         else if(Pred()(element.key, node->data.key))
+        {
             node->data.value = element.value;
+            return {0, 0};
+        }
         else if(Compare()(element.key, node->data.key))
-            insert(element, node->left);
+        {
+            Info node_left = insert(element, node->left, node);
+            if(node == prev || node_left == Info{0, 0})
+                return {0, 0};
+            else if(node->balance_factor == -2)
+            {
+                right_to_left()
+            }
+            else if(node->balance_factor == 2)
+            {
+
+            }
+            else
+            {
+                int node_right = node_left.cur_depth - node->balance_factor;
+                int node_cur_depth = std::max(node_left.cur_depth, node_right) + 1;
+                int node_prev_depth = std::max(node_left.prev_depth, node_right) + 1;
+
+                if(node == prev->left)
+                {
+                    int prev_right = node_prev_depth - prev->balance_factor;
+                    prev->balance_factor = node_cur_depth - prev_right;
+                }
+                else
+                {
+                    int prev_left = prev->balance_factor + node_prev_depth;
+                    prev->balance_factor = prev_left - node_cur_depth;
+                }
+
+                return {node_prev_depth, node_cur_depth};
+            }
+        }
         else
-            insert(element, node->right);
+        {
+            Info node_right = insert(element, node->right, node);
+            if(node == prev || node_right == Info{0, 0})
+                return {0, 0};
+            else
+            {
+                int node_left = node_right.cur_depth + node->balance_factor;
+                int node_cur_depth = std::max(node_right.cur_depth, node_left) + 1;
+                int node_prev_depth = std::max(node_right.prev_depth, node_left) + 1;
+
+                if(node == prev->left)
+                {
+                    int prev_right = node_prev_depth - prev->balance_factor;
+                    prev->balance_factor = node_cur_depth - prev_right;
+                }
+                else
+                {
+                    int prev_left = prev->balance_factor + node_prev_depth;
+                    prev->balance_factor = prev_left - node_cur_depth;
+                }
+
+                return {node_prev_depth, node_cur_depth};
+            }
+        }
+    }
+
+    int refresh(BlanceSortTree* node)
+    {
+        if(node == nullptr)
+            return 0;
+        int left = refresh(node->left);
+        int right = refresh(node->right);
+        node->balance_factor = left - right;
+        return std::max(left, right) + 1;
     }
 };
